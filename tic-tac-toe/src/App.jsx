5f4519bffeea6095039451c7d817da5c0c0bded6
Christian Harris <ChristianHarris1@proton.me>
2023-03-14 13:56:01 -0700

    Adding win checking method. Unexpected behavior: checkWin is called before the game state finishes updating

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -5,7 +5,35 @@ function App() {
   const [gameState, setGameState] = React.useState(
     Array(9).fill(null)
   )
+  const [gameOver, setGameOver] = React.useState(false);
   const [currentMoveValue, setCurrentMoveValue] = React.useState('X');
+
+  const possibleWinIndices = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+  ];
+
+  const checkWin = () => {
+    console.log(gameState);
+    possibleWinIndices.map((winIndices) => {
+      if (gameState[winIndices[0]] === 
+          gameState[winIndices[1]] === 
+          gameState[winIndices[2]]) {
+        setGameOver(true);
+      }
+    })
+    if (!gameOver) {
+      setCurrentMoveValue(
+        (currentMoveValue === 'X') ? 'O' : 'X'
+      )
+    }
+  };
 
   const onMakeMove = (boardSquareIndex) => {
     setGameState(gameState.map((element, index) => {
@@ -14,12 +42,13 @@ function App() {
       }
       return element;
     }))
-  }
+    checkWin();
+  };
 
   return (
     <Board gameState={gameState} onMakeMove={onMakeMove}/>
   )
-}
+};
 
 const Board = ({gameState, onMakeMove}) => (
   <div className='board'>
