824f2d64435c4460befa6f7e4604bd79761062d1
Christian Harris <ChristianHarris1@proton.me>
2023-03-14 14:42:51 -0700

    Fixed useState async behavior with useEffect. useEffect is now called during initial render which alters the initial move value

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,40 +1,40 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
+
+const possibleWinIndices = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [2,4,6]
+];
+
 
 function App() {
   const [gameState, setGameState] = React.useState(
     Array(9).fill(null)
   )
   const [gameOver, setGameOver] = React.useState(false);
-  const [currentMoveValue, setCurrentMoveValue] = React.useState('X');
-
-  const possibleWinIndices = [
-    [0,1,2],
-    [3,4,5],
-    [6,7,8],
-    [0,3,6],
-    [1,4,7],
-    [2,5,8],
-    [0,4,8],
-    [2,4,6]
-  ];
-
+  const [currentMoveValue, setCurrentMoveValue] = React.useState('O');
+  
   const checkWin = () => {
-    console.log(gameState);
     possibleWinIndices.map((winIndices) => {
       if (gameState[winIndices[0]] === 
-          gameState[winIndices[1]] === 
-          gameState[winIndices[2]]) {
+          gameState[winIndices[1]] &&
+          gameState[winIndices[1]] ===
+          gameState[winIndices[2]] &&
+          gameState[winIndices[2]] != null) {
         setGameOver(true);
       }
     })
     if (!gameOver) {
-      setCurrentMoveValue(
-        (currentMoveValue === 'X') ? 'O' : 'X'
-      )
+      setCurrentMoveValue((currentMoveValue === 'X') ? 'O' : 'X');
     }
   };
-
+  
   const onMakeMove = (boardSquareIndex) => {
     setGameState(gameState.map((element, index) => {
       if (boardSquareIndex === index) {
@@ -42,9 +42,12 @@ function App() {
       }
       return element;
     }))
-    checkWin();
   };
 
+  React.useEffect(() =>
+    checkWin()
+  , [gameState]);
+    
   return (
     <Board gameState={gameState} onMakeMove={onMakeMove}/>
   )
