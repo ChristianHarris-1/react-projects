ae95ef73aec332a72de24cff0179d24b1501cbe2
Christian Harris <ChristianHarris1@proton.me>
2023-03-14 11:32:02 -0700

    Adding a game state to track the values in each board square

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -2,33 +2,37 @@ import React, { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [gameState, setGameState] = React.useState(
+    Array(9).fill(1)
+  )
+
   return (
-    <Board/>
+    <Board gameState={gameState}/>
   )
 }
 
-const Board = () => (
+const Board = ({gameState}) => (
   <div className='board'>
     <div className='boardRow'>
-      <BoardSquare/>
-      <BoardSquare/>
-      <BoardSquare/>
+      <BoardSquare index={0} boardSquareValue={gameState[0]}/>
+      <BoardSquare index={1} boardSquareValue={gameState[1]}/>
+      <BoardSquare index={2} boardSquareValue={gameState[2]}/>
     </div>
     <div className='boardRow'>
-      <BoardSquare/>
-      <BoardSquare/>
-      <BoardSquare/>
+      <BoardSquare index={3} boardSquareValue={gameState[3]}/>
+      <BoardSquare index={4} boardSquareValue={gameState[4]}/>
+      <BoardSquare index={5} boardSquareValue={gameState[5]}/>
     </div>
     <div className='boardRow'>
-      <BoardSquare/>
-      <BoardSquare/>
-      <BoardSquare/>
+      <BoardSquare index={6} boardSquareValue={gameState[6]}/>
+      <BoardSquare index={7} boardSquareValue={gameState[7]}/>
+      <BoardSquare index={8} boardSquareValue={gameState[8]}/>
     </div>
   </div>
 );
 
-const BoardSquare = () => (
-  <div className='boardSquare'></div>
+const BoardSquare = ({index, boardSquareValue}) => (
+  <div className='boardSquare'>{boardSquareValue}</div>
 );
 
 export default App;
