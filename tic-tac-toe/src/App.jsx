0af0df3fd818c9e1887bfc9b0b33052cd18e2223
Christian Harris <ChristianHarris1@proton.me>
2023-03-14 12:14:02 -0700

    Adding a handler to update board square values

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -3,36 +3,67 @@ import './App.css'
 
 function App() {
   const [gameState, setGameState] = React.useState(
-    Array(9).fill(1)
+    Array(9).fill(null)
   )
+  const [currentMoveValue, setCurrentMoveValue] = React.useState('X');
+
+  const onMakeMove = (boardSquareIndex) => {
+    setGameState(gameState.map((element, index) => {
+      if (boardSquareIndex === index) {
+        return currentMoveValue;
+      }
+      return element;
+    }))
+  }
 
   return (
-    <Board gameState={gameState}/>
+    <Board gameState={gameState} onMakeMove={onMakeMove}/>
   )
 }
 
-const Board = ({gameState}) => (
+const Board = ({gameState, onMakeMove}) => (
   <div className='board'>
     <div className='boardRow'>
-      <BoardSquare index={0} boardSquareValue={gameState[0]}/>
-      <BoardSquare index={1} boardSquareValue={gameState[1]}/>
-      <BoardSquare index={2} boardSquareValue={gameState[2]}/>
+      <BoardSquare boardSquareIndex={0} 
+                    boardSquareValue={gameState[0]}
+                    makeMove={onMakeMove}/>
+      <BoardSquare boardSquareIndex={1}
+                    boardSquareValue={gameState[1]}
+                    makeMove={onMakeMove}/>
+      <BoardSquare boardSquareIndex={2}
+                    boardSquareValue={gameState[2]}
+                    makeMove={onMakeMove}/>
     </div>
     <div className='boardRow'>
-      <BoardSquare index={3} boardSquareValue={gameState[3]}/>
-      <BoardSquare index={4} boardSquareValue={gameState[4]}/>
-      <BoardSquare index={5} boardSquareValue={gameState[5]}/>
+      <BoardSquare boardSquareIndex={3} 
+                    boardSquareValue={gameState[3]}
+                    makeMove={onMakeMove}/>
+      <BoardSquare boardSquareIndex={4}
+                    boardSquareValue={gameState[4]}
+                    makeMove={onMakeMove}/>
+      <BoardSquare boardSquareIndex={5}
+                    boardSquareValue={gameState[5]}
+                    makeMove={onMakeMove}/>
     </div>
     <div className='boardRow'>
-      <BoardSquare index={6} boardSquareValue={gameState[6]}/>
-      <BoardSquare index={7} boardSquareValue={gameState[7]}/>
-      <BoardSquare index={8} boardSquareValue={gameState[8]}/>
+      <BoardSquare boardSquareIndex={6} 
+                    boardSquareValue={gameState[6]}
+                    makeMove={onMakeMove}/>
+      <BoardSquare boardSquareIndex={7}
+                    boardSquareValue={gameState[7]}
+                    makeMove={onMakeMove}/>
+      <BoardSquare boardSquareIndex={8}
+                    boardSquareValue={gameState[8]}
+                    makeMove={onMakeMove}/>
     </div>
   </div>
 );
 
-const BoardSquare = ({index, boardSquareValue}) => (
-  <div className='boardSquare'>{boardSquareValue}</div>
+const BoardSquare = ({boardSquareIndex, boardSquareValue, makeMove}) => (
+  <div className='boardSquare'
+        onClick={() => makeMove(boardSquareIndex)}>
+          {boardSquareValue}
+  </div>
 );
 
 export default App;
