f822d3a14a216895ec185e3d97ad5d5caa82383b
Christian Harris <ChristianHarris1@proton.me>
2023-03-14 10:49:16 -0700

    Adding a basic 3x3 board

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
-    null
+    <Board/>
   )
 }
 
-export default App
+const Board = () => (
+  <div className='board'>
+    <div className='boardRow'>
+      <BoardSquare/>
+      <BoardSquare/>
+      <BoardSquare/>
+    </div>
+    <div className='boardRow'>
+      <BoardSquare/>
+      <BoardSquare/>
+      <BoardSquare/>
+    </div>
+    <div className='boardRow'>
+      <BoardSquare/>
+      <BoardSquare/>
+      <BoardSquare/>
+    </div>
+  </div>
+);
+
+const BoardSquare = () => (
+  <div className='boardSquare'></div>
+);
+
+export default App;
