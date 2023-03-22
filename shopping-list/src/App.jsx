19f64562359b40fa741d45bd0e853ae52f88a794
Christian Harris <ChristianHarris1@proton.me>
2023-03-22 13:40:30 -0700

    Adding a static wireframe with SearchBar, ShoppingList, and ListItem components

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -2,7 +2,38 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  return (
+    <>
+      <h1 className="title">Shopping List</h1>
+      <SearchBar/>
+      <ShoppingList/>   
+    </>
+  )
 }
+
+const SearchBar = () => (
+  <form className="search">
+    <input type="text" className="searchInput" placeholder="Search. . ."/>
+    <input type="text" className="quantityInput" placeholder="How many?" default={1}/>
+    <button type="submit" className="addButton">Add</button>
+  </form>
+)
+
+const ShoppingList = () => (
+  <div className="shoppingList">
+    <ListItem itemName="Item1" quantity="1"/>
+    <ListItem itemName="Item2" quantity="2"/>
+    <ListItem itemName="Item3" quantity="3"/>
+  </div>
+)
+
+const ListItem = ({itemName, quantity}) => (
+  <div className="listItem">
+    <p className="itemName">{itemName}</p>
+    <p className="itemQuantity">{quantity}</p>
+    <button className="editButton">Edit</button>
+    <button className="exitButton">Exit</button>
+  </div>
+);
 
 export default App
