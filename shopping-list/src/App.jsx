b95ae9ac548632740d6249e96be6ee964132b88c
Christian Harris <ChristianHarris1@proton.me>
2023-03-22 14:24:12 -0700

    Creating shoppingList and searchInput states for managing user interactions then passing them as props to the respective child components.

--- shopping-list/src/App.jsx
+++ shopping-list/src/App.jsx
@@ -1,39 +1,44 @@
-import { useState } from 'react'
+import * as React from 'react';
 import './App.css'
 
+// {itemName:, itemQuantity:}
+
 function App() {
+  const [shoppingList, setShoppingList] = React.useState([]);
+  const [searchInput, setSearchInput] = React.useState("");
+
   return (
     <>
       <h1 className="title">Shopping List</h1>
-      <SearchBar/>
-      <ShoppingList/>   
+      <SearchBar searchInput={searchInput}/>
+      <ShoppingList shoppingList={shoppingList}/>   
     </>
   )
 };
 
-const SearchBar = () => (
+const SearchBar = ({searchInput}) => (
   <form className="search">
-    <input type="text" className="searchInput" placeholder="Search. . ."/>
-    <input type="text" className="quantityInput" placeholder="How many?" default={1}/>
+    <input type="text" className="searchInput" placeholder="Search. . ." value={searchInput}/>
+    <input type="text" className="quantityInput" placeholder="How many?"/>
     <button type="submit" className="addButton">Add</button>
   </form>
 );
 
-const ShoppingList = () => (
-  <div className="shoppingList">
-    <ListItem itemName="Item1" quantity="1"/>
-    <ListItem itemName="Item2" quantity="2"/>
-    <ListItem itemName="Item3" quantity="3"/>
-  </div>
+const ShoppingList = ({shoppingList}) => (
+  <ul className="shoppingList">
+    {shoppingList.map((listItem) => {
+      <ListItem itemName={listItem.itemName} quantity={listItem.quantity}/>
+    })}
+  </ul>
 );
 
 const ListItem = ({itemName, quantity}) => (
-  <div className="listItem">
+  <li className="listItem">
     <p className="itemName">{itemName}</p>
     <p className="itemQuantity">{quantity}</p>
     <button className="editButton">Edit</button>
     <button className="removeButton">X</button>
-  </div>
+  </li>
 );
 
 export default App
